c3b455cd158ce3a42c06119b846852843f77fbd3
Ivandlabeuh <66721716+Ivandlabeuh@users.noreply.github.com>
2022-08-22 18:16:11 +0200

    Delete twitch-spotify-request-bot-main directory

--- twitch-spotify-request-bot-main/lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  '*.{js,ts}': [
-    'eslint --fix -c .eslintrc.js --ext',
-    'prettier -c .prettierrc.js --write',
-  ],
-  '*.{yml,yaml,md,json}': ['prettier -c .prettierrc.js --write'],
-};
